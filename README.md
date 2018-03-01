44fdccfcc60f1de79a03ee63ed54f6054dc5fd5f
Sarit Ritwirune <sarit@elcolie.com>
2018-03-01 14:55:59 +0700

    Ready

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## HT6MInterface
 A simple webserver to communicate with Fortran program
 
+## How to run in development mode
+$ export FLASK_APP=main.py
+$ export FLASK_DEBUG=1
+$ flask run
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, World!'
+    if request.method == 'GET':
+        return render_template('hello.html')
