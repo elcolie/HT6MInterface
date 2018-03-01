28373379d3f9bca6d6c60ec577bb62acd7a63648
Sarit Ritwirune <sarit@elcolie.com>
2018-03-01 17:27:44 +0700

    Setup more boiler plates

--- f1/src/App.js
+++ f1/src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import {Provider} from 'react-redux';
 import './App.css';
 import InputInterface from "./InputInterface/components/input-interface";
+import rootReducer from "./reducers";
+import {applyMiddleware, createStore} from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas';
+
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleWare)
+);
+sagaMiddleWare.run(rootSaga);
 
 class App extends Component {
   render() {
     return (
-      <InputInterface/>
+      <Provider store={store}>
+        <InputInterface/>
+      </Provider>
     )
   }
 }
