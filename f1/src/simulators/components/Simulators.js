20361f828045cff42304f80b6678de58da8643f0
Sarit Ritwirune <sarit@elcolie.com>
2018-05-30 20:21:43 +0700

    Attacking `NavBar`

--- f1/src/simulators/components/Simulators.js
+++ f1/src/simulators/components/Simulators.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import Footer from "../../commons/components/Footer";
 import Header from "../../commons/components/Headers";
+import {Navbar, Nav, NavItem, NavDropdown} from 'reactstrap'
 
 class Simulator extends Component{
   render(){
