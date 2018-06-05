2c071582f29376c4e215a100c2608519e8172562
Sarit Ritwirune <sarit@elcolie.com>
2018-06-05 23:18:31 +0700

    Add `props` and method to `props`. Intermediate ControlRoom

--- f1/src/controlroom/components/Intermediate.js
+++ f1/src/controlroom/components/Intermediate.js
@@ -1,13 +1,33 @@
 import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
 
 class Intermediate extends Component {
+  handleSubmit = (event, payload) => {
+    console.log(`handleSubmit`);
+    console.log(this.state);  //I will emit `action` here
+    this.props.submitIntermediateForm(this.state);
+  };
+  
   render() {
     return (
       <Fragment>
-        Intermediate Control Room
+        <h1>Step 1/5: Set machine's parameters</h1>
+        <img src={'mach_params.png'} className={'intermediate-image'}/>
+      
       </Fragment>
     )
   }
 }
 
-export default Intermediate;
+const submitIntermediateForm = (data) => {
+  return {
+    type: SUBMIT_INTERMEIATE_FORM,
+    payload: data
+  }
+};
+
+const mapStateToProps = (newProps, ownProps) => {
+  return newProps;
+};
+
+export default connect(mapStateToProps, {submitIntermediateForm})(Intermediate);
