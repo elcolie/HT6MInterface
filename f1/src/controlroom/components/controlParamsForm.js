2ca7a1b0c7e9eaf2ea31593579ae2c79fb020836
Sarit Ritwirune <sarit@elcolie.com>
2018-06-26 21:58:38 +0700

    Get back to old design

--- f1/src/controlroom/components/controlParamsForm.js
+++ f1/src/controlroom/components/controlParamsForm.js
@@ -17,7 +17,7 @@ const Step = Steps.Step;
 const dumpParticleAndHeatSources = (props) => {
 	const {steps, state} = props;
 	return (
-		<div className="steps-content">{steps[state.current].content}</div>
+			<div className="steps-content">{steps[state.current].content}</div>
 	)
 };
 
@@ -37,54 +37,54 @@ class ControlParameterForm extends Component {
 		const {current} = this.state;
 		const {steps} = this.props.controlParametersReducer;
 		return (
-			<Fragment>
+				<Fragment>
 
-				<SimpleStep controlParametersReducer={this.props.controlParametersReducer}/>
+					<SimpleStep controlParametersReducer={this.props.controlParametersReducer}/>
 
-				<Form className='justify-center'>
-					<table>
-						<tbody>
-						<tr>
-							<td className='test'><b>Number of time break points</b></td>
-							<td><Form.Select id='control-dropdown' name='numberOfTimeBreakpoints' fluid
-															 options={NUMBER_OF_TIME_BREAK_POINTS_OPTIONS}
-															 placeholder={NUMBER_OF_TIME_BREAK_POINTS_DEFAULT} onChange={(event, value) => {
-								this.props.onChangeNumberOfBreakPoints(value);  //No idea why dispatch already but I have to set state again
-								this.clickBreakPoint(0);  //Reset it to zero otherwise `undefined` by index out bound
-							}}/></td>
-						</tr>
-						<tr>
-							<td className='padding-right'><b>Maximum run time (s)</b></td>
-							<td><Form.Select id='control-dropdown' name='maximumRunTime' fluid options={MAXIMUM_RUNTIME_OPTIONS}
-															 placeholder={MAXIMUM_RUNTIME_DEFAULT} onChange={(event, value) => {
-								this.props.onChangeMaximumRunTime(value); //Same reason as above. It might be a Semantic way
-							}}/></td>
-						</tr>
-						</tbody>
-					</table>
-				</Form>
+					<Form className='justify-center'>
+						<table>
+							<tbody>
+							<tr>
+								<td className='test'><b>Number of time break points</b></td>
+								<td><Form.Select id='control-dropdown' name='numberOfTimeBreakpoints' fluid
+																 options={NUMBER_OF_TIME_BREAK_POINTS_OPTIONS}
+																 placeholder={NUMBER_OF_TIME_BREAK_POINTS_DEFAULT} onChange={(event, value) => {
+									this.props.onChangeNumberOfBreakPoints(value);  //No idea why dispatch already but I have to set state again
+									this.clickBreakPoint(0);  //Reset it to zero otherwise `undefined` by index out bound
+								}}/></td>
+							</tr>
+							<tr>
+								<td className='padding-right'><b>Maximum run time (s)</b></td>
+								<td><Form.Select id='control-dropdown' name='maximumRunTime' fluid options={MAXIMUM_RUNTIME_OPTIONS}
+																 placeholder={MAXIMUM_RUNTIME_DEFAULT} onChange={(event, value) => {
+									this.props.onChangeMaximumRunTime(value); //Same reason as above. It might be a Semantic way
+								}}/></td>
+							</tr>
+							</tbody>
+						</table>
+					</Form>
 
-				<div id='back-next-text-center' className='back-next-button'>
-					<a href="#transport-params" className="btn btn-info" role="button">Back</a>
-					<a href="#set-particle-and-heatsources-params" id='back-next-button-spacing' className="btn btn-info"
-						 role="button">Next</a>
-				</div>
+					<div id='back-next-text-center' className='back-next-button'>
+						<a href="#transport-params" className="btn btn-info" role="button">Back</a>
+						<a href="#set-particle-and-heatsources-params" id='back-next-button-spacing' className="btn btn-info"
+							 role="button">Next</a>
+					</div>
 
-				<h1 id={'set-particle-and-heatsources-params'}>
-					Step 5/5: Set particle and heat sources at each time break
-					points
-				</h1>
+					<h1 id={'set-particle-and-heatsources-params'}>
+						Step 5/5: Set particle and heat sources at each time break
+						points
+					</h1>
 
-				<div>
-					<Steps current={current}>
-						{steps.map((item, index) => <Step key={index} onClick={() => {
-							this.clickBreakPoint(index);
-						}}/>)}
-					</Steps>
-				</div>
-				{dumpParticleAndHeatSources({steps, state: this.state})}
+					<div>
+						<Steps current={current}>
+							{steps.map((item, index) => <Step key={index} onClick={() => {
+								this.clickBreakPoint(index);
+							}}/>)}
+						</Steps>
+					</div>
+					{dumpParticleAndHeatSources({steps, state: this.state})}
 
-			</Fragment>
+				</Fragment>
 		)
 	}
 }
