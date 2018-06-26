84eb2f2cc627ca607faf3edbad8608629fb545a6
Sarit Ritwirune <sarit@elcolie.com>
2018-06-26 23:39:52 +0700

    Try `Grid` from `sematic`

--- f1/src/controlroom/components/particleHeatForm.js
+++ f1/src/controlroom/components/particleHeatForm.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react';
 import {Form} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {SPECIES} from "../../constants";
+import { Grid, Segment } from 'semantic-ui-react'
 
 const dumpParticleHeatForm = (props) => {
   const {label, placeholder, unit} = props;
@@ -23,45 +24,68 @@ class ParticleHeatForm extends Component {
   render() {
     return (
       <Fragment>
-        <label>Break point number {this.props.breakPointNumber}</label>
-        <br/>
-        <label>Time at this break point</label>
-        
-        <div className="ui right labeled input">
-          <input type="text" placeholder="0.01"/>
-          <div className="ui basic label">
-            ms
-          </div>
-        </div>
-        
-        <fieldset id='login-fieldset'>
-          <legend id='login-legend'>Particle Source</legend>
-          <Form>
-            <Form.Group inline>
-              <label>Ion species of the source</label>
-              <Form.Select name={'ionSpeciesOfTheSource'} placeholder={SPECIES[1].text} fluid options={SPECIES}/>
-            </Form.Group>
-            
-            {dumpParticleHeatForm({label: "Rate of particle source", placeholder: "0.01", unit: 'ms'})}
-            {dumpParticleHeatForm({label: "Radial position", placeholder: "0.01", unit: 'm'})}
-            {dumpParticleHeatForm({label: "Radial width", placeholder: "0.50", unit: 'm'})}
-          
-          </Form>
-        </fieldset>
-        
-        <fieldset id='login-fieldset'>
-          <legend id='login-legend'>Heat Source</legend>
-          <Form>
-            {dumpParticleHeatForm({label: "NBI total power", placeholder: "0.01", unit: 'W'})}
-            {dumpParticleHeatForm({label: "NBI radial position", placeholder: "0.01", unit: 'm'})}
-            {dumpParticleHeatForm({label: "NBI radial width", placeholder: "0.01", unit: 'm'})}
-            
-            {dumpParticleHeatForm({label: "ICRF total power", placeholder: "0.01", unit: 'W'})}
-            {dumpParticleHeatForm({label: "ICRF radial position", placeholder: "0.01", unit: 'm'})}
-            {dumpParticleHeatForm({label: "ICRF radial width", placeholder: "0.01", unit: 'm'})}
-          
-          </Form>
-        </fieldset>
+        <Grid textAlign='left'>
+          <Grid.Row columns={2}>
+            <Grid.Column width={3}>
+              <label>Break point number</label>
+            </Grid.Column>
+            <Grid.Column width={2}>
+              <label>{this.props.breakPointNumber}</label>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row columns={2}>
+            <Grid.Column width={3}>
+              <label className='grid-time-breakpoint'>Time at this break point</label>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <div className="ui right labeled input">
+              <input type="text" placeholder="0.01"/>
+                <div className="ui basic label">
+                  ms
+                </div>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row columns={2}>
+            <Grid.Column >
+              <fieldset id='login-fieldset'>
+                <legend id='login-legend'>Particle Source</legend>
+                <Form>
+                  <Form.Group inline>
+                    <label>Ion species of the source</label>
+                    <Form.Select name={'ionSpeciesOfTheSource'} placeholder={SPECIES[1].text} fluid options={SPECIES}/>
+                  </Form.Group>
+
+                  {dumpParticleHeatForm({label: "Rate of particle source", placeholder: "0.01", unit: 'ms'})}
+                  {dumpParticleHeatForm({label: "Radial position", placeholder: "0.01", unit: 'm'})}
+                  {dumpParticleHeatForm({label: "Radial width", placeholder: "0.50", unit: 'm'})}
+
+                </Form>
+              </fieldset>
+            </Grid.Column>
+            <Grid.Column>
+              <fieldset id='login-fieldset'>
+                <legend id='login-legend'>Heat Source</legend>
+                <Form>
+                  {dumpParticleHeatForm({label: "NBI total power", placeholder: "0.01", unit: 'W'})}
+                  {dumpParticleHeatForm({label: "NBI radial position", placeholder: "0.01", unit: 'm'})}
+                  {dumpParticleHeatForm({label: "NBI radial width", placeholder: "0.01", unit: 'm'})}
+
+                  {dumpParticleHeatForm({label: "ICRF total power", placeholder: "0.01", unit: 'W'})}
+                  {dumpParticleHeatForm({label: "ICRF radial position", placeholder: "0.01", unit: 'm'})}
+                  {dumpParticleHeatForm({label: "ICRF radial width", placeholder: "0.01", unit: 'm'})}
+
+                </Form>
+              </fieldset>
+            </Grid.Column>
+          </Grid.Row>
+
+
+
+        </Grid>
+
         <a href="#control-params" className="btn btn-info" role="button">Back</a>
         <a href="#confirmation" className="btn btn-info" role="button">Next</a>
       
