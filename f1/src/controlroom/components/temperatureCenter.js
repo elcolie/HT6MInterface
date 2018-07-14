420dbe8bd6dd7ab14073c25a0674a63cdeae4f4b
Sarit Ritwirune <sarit@elcolie.com>
2018-07-14 14:40:18 +0700

    `TemperatureAtCenter` wired with action creator

--- f1/src/controlroom/components/temperatureCenter.js
+++ f1/src/controlroom/components/temperatureCenter.js
@@ -1,34 +1,62 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Input, Table} from 'semantic-ui-react';
+import {SPECIE_CHANGE_DT} from "../../constants";
 
 class TemperatureAtCenter extends Component {
-  render() {
-    const {
-      props: {
-        specieReducer: {nsmax}
-      }
-    } = this.props;
-    return (
-      <Table.Row>
-        <Table.Cell>Temperature at the center</Table.Cell>
-        <Table.Cell>
-          <Input type='number' name={'electronTemperatureCenter'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input type='number' name={'hydrogenTemperatureCenter'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input disabled={nsmax < 3} type='number' name={'deuteriumTemperatureCenter'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input disabled={nsmax < 4} type='number' name={'TritiumTemperatureCenter'} placeholder={'0.00'}/>
-        </Table.Cell>
-      </Table.Row>
-    )
-  }
+	render() {
+		const {
+			props: {
+				specieReducer: {nsmax}
+			}
+		} = this.props;
+		this.key = 'tempAtCenter';
+		return (
+				<Table.Row>
+					<Table.Cell>Temperature at the center</Table.Cell>
+					<Table.Cell>
+						<Input type='number' name={'electronTemperatureCenter'} placeholder={0.00} onChange={(event) => {
+							this.props.specieChangeDT('electron', this.key, Number(event.target.value));
+						}}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input type='number' name={'hydrogenTemperatureCenter'} placeholder={0.00} onChange={(event) => {
+							this.props.specieChangeDT('hydrogen', this.key, Number(event.target.value));
+						}}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input disabled={nsmax < 3} type='number' name={'deuteriumTemperatureCenter'} placeholder={0.00}
+									 onChange={(event) => {
+										 this.props.specieChangeDT('deuterium', this.key, Number(event.target.value));
+									 }}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input disabled={nsmax < 4} type='number' name={'TritiumTemperatureCenter'} placeholder={0.00}
+									 onChange={(event) => {
+										 this.props.specieChangeDT('tritium', this.key, Number(event.target.value));
+									 }}/>
+					</Table.Cell>
+				</Table.Row>
+		)
+	}
 }
 
-export default TemperatureAtCenter;
+const specieChangeDT = (specie, key, value) => {
+	return {
+		type: SPECIE_CHANGE_DT,
+		payload: {
+			specie,
+			key,
+			value
+		}
+	}
+};
+
+const mapStateToProps = (newProps, currProps) => {
+	return newProps;
+};
+
+export default connect(mapStateToProps, {specieChangeDT})(TemperatureAtCenter);
