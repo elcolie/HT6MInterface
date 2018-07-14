2dd4ccf86985e8f548912ef1c3d74ee280211db1
Sarit Ritwirune <sarit@elcolie.com>
2018-07-14 14:31:34 +0700

    `DensityTemperatureContainer` wired with action creator

--- f1/src/controlroom/components/densityTemperature.js
+++ f1/src/controlroom/components/densityTemperature.js
@@ -1,36 +1,64 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Input, Table} from 'semantic-ui-react';
+import {SPECIE_CHANGE_DT} from "../../constants";
 
 class DensityTemperatureContainer extends Component {
-  
-  render() {
-    const {
-      props: {
-        specieReducer: {nsmax}
-      }
-    } = this.props;
-    
-    return (
-      <Table.Row>
-        <Table.Cell>Density of the edge</Table.Cell>
-        <Table.Cell>
-          <Input type='number' name={'electronDensityEdge'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input type='number' name={'hydrogenDensityEdge'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input disabled={nsmax < 3} type='number' name={'deuteriumDensityEdge'} placeholder={'0.00'}/>
-        </Table.Cell>
-        
-        <Table.Cell>
-          <Input disabled={nsmax < 4} type='number' name={'TritiumDensityEdge'} placeholder={'0.00'}/>
-        </Table.Cell>
-      </Table.Row>
-    )
-  }
+
+	render() {
+		const {
+			props: {
+				specieReducer: {nsmax}
+			}
+		} = this.props;
+		this.key = 'densityOfEdge';
+		return (
+				<Table.Row>
+					<Table.Cell>Density of the edge</Table.Cell>
+					<Table.Cell>
+						<Input type='number' name={'electronDensityEdge'} placeholder={0.00} onChange={(event) => {
+							this.props.specieChangeDT('electron', this.key, Number(event.target.value));
+						}}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input type='number' name={'hydrogenDensityEdge'} placeholder={0.00} onChange={(event) => {
+							this.props.specieChangeDT('hydrogen', this.key, Number(event.target.value));
+						}}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input disabled={nsmax < 3} type='number' name={'deuteriumDensityEdge'} placeholder={0.00}
+									 onChange={(event) => {
+										 this.props.specieChangeDT('deuterium', this.key, Number(event.target.value));
+									 }}/>
+					</Table.Cell>
+
+					<Table.Cell>
+						<Input disabled={nsmax < 4} type='number' name={'TritiumDensityEdge'} placeholder={0.00}
+									 onChange={(event) => {
+										 this.props.specieChangeDT('tritium', this.key, Number(event.target.value));
+									 }}/>
+					</Table.Cell>
+				</Table.Row>
+		)
+	}
 }
 
-export default DensityTemperatureContainer;
+const specieChangeDT = (specie, key, value) => {
+	return {
+		type: SPECIE_CHANGE_DT,
+		payload: {
+			specie,
+			key,
+			value
+		}
+	}
+};
+
+const mapStateToProps = (newProps, currProps) => {
+	return newProps;
+};
+
+
+export default connect(mapStateToProps, {specieChangeDT})(DensityTemperatureContainer);
